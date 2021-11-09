d373530fcd6417848cd63770a7732efe2fa6c064
zanderle <zan.anderle@gmail.com>
2021-11-09 17:34:12 +0100

    Add a path for home page

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("", include("django_starter_buttercms.content.urls", namespace="content")),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
--- django_starter_buttercms/content/urls.py
+++ django_starter_buttercms/content/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from .views import ButterCMSPageView
 
-app_name = "users"
+app_name = "content"
 urlpatterns = [
+    # The default home page is under "landing-page-with-components" slug, so let's add a root path to reflect that
+    path("", ButterCMSPageView.as_view(), {"slug": "landing-page-with-components"}, name="buttercms_view"),
+    # And a path for all the other slugs, that can be accessed directly
     path("<str:slug>/", view=ButterCMSPageView.as_view(), name="buttercms_view"),
 ]
