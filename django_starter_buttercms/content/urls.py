012b92d246fd1cebbb0297a32994ae029a5a55be
Uros Trebec <uros@buttercms.com>
2021-11-17 01:30:23 +0100

    Add URL for `ButterCMSBlogSearchView`
    And some black formatting.

--- django_starter_buttercms/content/urls.py
+++ django_starter_buttercms/content/urls.py
@@ -1,16 +1,33 @@
 from django.urls import path
 
-from .views import ButterCMSPageView, ButterCMSBlogView, ButterCMSBlogPostView
+from .views import (
+    ButterCMSBlogPostView,
+    ButterCMSBlogSearchView,
+    ButterCMSBlogView,
+    ButterCMSPageView,
+)
 
 app_name = "content"
 urlpatterns = [
     # The default home page is under "landing-page-with-components" slug, so let's add a root path to reflect that
-    path("", ButterCMSPageView.as_view(), {"slug": "landing-page-with-components"}, name="home"),
+    path(
+        "",
+        ButterCMSPageView.as_view(),
+        {"slug": "landing-page-with-components"},
+        name="home",
+    ),
     # Blog paths
     path("blog/", ButterCMSBlogView.as_view(), name="blog"),
+    path("blog/search/", ButterCMSBlogSearchView.as_view(), name="blog_search"),
     path("blog/<str:slug>/", view=ButterCMSBlogPostView.as_view(), name="blog_post"),
-    path("blog/category/<str:category_slug>/", view=ButterCMSBlogView.as_view(), name="blog_category"),
-    path("blog/tag/<str:tag_slug>/", view=ButterCMSBlogView.as_view(), name="blog_tags"),
+    path(
+        "blog/category/<str:category_slug>/",
+        view=ButterCMSBlogView.as_view(),
+        name="blog_category",
+    ),
+    path(
+        "blog/tag/<str:tag_slug>/", view=ButterCMSBlogView.as_view(), name="blog_tags"
+    ),
     # And a path for all the other slugs, that can be accessed directly
     path("<str:slug>/", view=ButterCMSPageView.as_view(), name="buttercms_view"),
 ]
