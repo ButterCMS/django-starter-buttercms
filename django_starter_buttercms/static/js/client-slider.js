d3e415bf239f2cb64795f7cc60d43a538ef32da1
Orly Shifman <orly.knop@gmail.com>
2025-06-08 18:58:26 +0300

    Activate slider only if element exists

--- django_starter_buttercms/static/js/client-slider.js
+++ django_starter_buttercms/static/js/client-slider.js
@@ -1,19 +1,20 @@
 (function() {
-    tns({
-        container: '.testimonial-active',
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayButtonOutput: false,
-        mouseDrag: true,
-        gutter: 0,
-        nav: false,
-        navPosition: "bottom",
-        controls: true,
-        controlsText: [
-            '<i class="lni lni-chevron-left"></i>',
-            '<i class="lni lni-chevron-right"></i>',
-        ],
-        items: 1,
-      });
-
+    if ( document.querySelector('.testimonial-active') ) {
+        tns({
+            container: '.testimonial-active',
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayButtonOutput: false,
+            mouseDrag: true,
+            gutter: 0,
+            nav: false,
+            navPosition: 'bottom',
+            controls: true,
+            controlsText: [
+                '<i class="lni lni-chevron-left"></i>',
+                '<i class="lni lni-chevron-right"></i>',
+            ],
+            items: 1,
+        });
+    }   
 })();
